a1f4bf0db7d90ad92e180415cf7afeefef7445cf
Andrea Peralta <andreacpb90@hotmail.com>
2025-06-01 00:07:59 -0500

    chore: add rotating file handler to manage log size

--- app/core/logger.py
+++ app/core/logger.py
@@ -1,13 +1,20 @@
 import logging
 import os
+from logging.handlers import RotatingFileHandler
 
 os.makedirs("logs", exist_ok=True)
+
+file_handler = RotatingFileHandler(
+    "logs/data_migration.log",
+    maxBytes=1_000_000,
+    backupCount=3
+)
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
     handlers=[
-        logging.FileHandler("logs/data_migration.log"),
+        file_handler,
         logging.StreamHandler()
     ]
 )

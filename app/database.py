2c78de55eb62ee0268dccf9a957cf4119268ca69
Andrea Peralta <andreacpb90@hotmail.com>
2025-06-01 02:30:40 -0500

    fix: correct type annotation for get_db dependency using Generator

--- app/database.py
+++ app/database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
+from typing import Generator
+
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
 
@@ -9,7 +11,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-def get_db() -> Session:
+def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
         yield db

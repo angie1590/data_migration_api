027b23c9afea2a19d801489ef31c73e59e3f2625
Andrea Peralta <andreacpb90@hotmail.com>
2025-06-01 01:39:20 -0500

    feat: foreign keys before insertion into hired_employees table

--- app/loaders/hire_employee_loader.py
+++ app/loaders/hire_employee_loader.py
@@ -1,14 +1,23 @@
 from datetime import datetime
 from sqlalchemy.orm import Session
-from app.models import HiredEmployee
+from app.models import HiredEmployee, Department, Job
 from app.schemas import HiredEmployeeCreate
 from app.loaders.base_loader import load_csv_generic
 
-def validate_hired_employee(row):
-    try:
-        datetime.strptime(row["datetime"], "%Y-%m-%dT%H:%M:%SZ")
-    except ValueError:
-        raise ValueError(f"Invalid datetime format: {row['datetime']}")
+def get_hired_employee_validator(session: Session):
+    def validate_hired_employee(row):
+        try:
+            datetime.strptime(row["datetime"], "%Y-%m-%dT%H:%M:%SZ")
+        except ValueError:
+            raise ValueError(f"Invalid datetime format: {row['datetime']}")
+
+        if not session.get(Department, row["department_id"]):
+            raise ValueError(f"Department ID {row['department_id']} does not exist")
+
+        if not session.get(Job, row["job_id"]):
+            raise ValueError(f"Job ID {row['job_id']} does not exist")
+
+    return validate_hired_employee
 
 def load_hired_employees(session: Session):
     load_csv_generic(
@@ -16,5 +25,5 @@ def load_hired_employees(session: Session):
         csv_path="data/hired_employees (1).csv",
         model_class=HiredEmployee,
         schema_class=HiredEmployeeCreate,
-        validator=validate_hired_employee
+        validator=get_hired_employee_validator(session)
     )
